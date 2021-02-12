6de91d9c0f9a91789a8d260c6690f3e8fa4e61c7
saraasqalan <asqalansara@gmail.com>
2021-02-12 22:08:25 +0200

    problem3 solved

--- app.js
+++ app.js
@@ -52,12 +52,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) {
+function sumAndMultiply(a ,b, c) {
+  let sum = a +b +c ;
+  let multiply = a*b*c ;
+  return [sum,multiply, a + ' and ' +  b + ' and ' + c + ' sum' + ' to ' + sum + '.' , 'The product of ' + a + ' and ' + b + ' and ' + c +' is ' + multiply + '.'];
+
   //eslint-disable-line
 }
+console.log(sumAndMultiply(4 ,7, 5));
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
